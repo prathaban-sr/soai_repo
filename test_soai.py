22c49cb99fd1825ffa5f1baf5c3b7905b04923c8
prathaban-sr <prathaban@gmail.com>
2021-07-04 01:46:31 +0530

    updated

--- test_soai.py
+++ test_soai.py
@@ -1,29 +1,9 @@
 from soai import ModelTrainer
 
 trainer = ModelTrainer()
-start_epoch = 0
-train_loss = []
-train_acc = []
-test_loss = []
-test_acc = []
+epoch = 5
+trainer.train_test(epoch)
+trainer.show_accuracy_loss_graphs()
 
-'''
-for epoch in range(start_epoch, start_epoch+200):
-    train_loss, train_acc = trainer.train(epoch)
-    test_loss, test_acc = trainer.test(epoch)
-    trainer.scheduler.step()
-
-import matplotlib.pyplot as plt
-
-fig, axs = plt.subplots(2,2,figsize=(15,10))
-axs[0, 0].plot(train_losses)
-axs[0, 0].set_title("Training Loss")
-axs[1, 0].plot(train_accs)
-axs[1, 0].set_title("Training Accuracy")
-axs[0, 1].plot(test_losses)
-axs[0, 1].set_title("Test Loss")
-axs[1, 1].plot(test_accs)
-axs[1, 1].set_title("Test Accuracy")
-'''
-
-trainer.showErrors()
+trainer.show_errors()
+trainer.show_attention()
